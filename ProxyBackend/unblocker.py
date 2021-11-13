164ec7a43320789f5bf88ddc3247d6503ea4a383
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-11-13 13:42:09 -0500

    working on backend server

--- ProxyBackend/unblocker.py
+++ ProxyBackend/unblocker.py
@@ -1,34 +1,52 @@
-import socket
+# --------------------------------------------------- GET DEPENDENCIES -----------------------------------------------
+try:
+    import socket
+    import threading
+except:
+    import pip
+    pip.main(['install', 'sockets', 'threading'])
+    import socket
+    import threading
 
-HEADER = 64
-PORT = 9091
-SERVER = '45.79.223.43'
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-
-proxy_client = socket.socket()
-
-
-def get(url):
-    print('[*] CONNECTING TO SERVER...')
-    proxy_client.connect(ADDR)
-    print('[*] REQUESTING WEBSITE...')
-    proxy_client.send(url.encode(FORMAT))
-    print('[*] GETTING RESPONSE...')
-    response = proxy_client.recv(3000)
-    print('[*] PROCCESSING RESPONSE...')
-    print(response.decode())
-    proxy_client.close()
+# --------------------------------------------------- CLASS DECLARATIONS -----------------------------------------------
 
 
-while True:
-    request = input("> ")
-    # if 'www.' in request:
-    #     headers = f"""GET http://{request} HTTPS/1.1
-    #                     Host: {request}\r\n\r\n"""
-    # else:
-    #     headers = f"""GET http://www.{request} HTTPS/1.1
-    #                     Host: www.{request}\r\n\r\n"""
-    headers = request
-    get(headers)
+class Proxy:
+    clients = threading.activeCount() - 1
+
+    def __init__(self, port):
+        """ INIT class: """
+        self.server_ip = socket.gethostbyname(socket.gethostname())
+        self.server = socket.socket()
+        self.port = port
+        self.address = (self.server_ip, self.port)
+
+    def initialize(self):
+        """ This function lets you create your own server inside a mini machine, please use the ip provided properly."""
+
+        try:
+            self.server.bind(self.address)
+            return self.address
+        except:
+            return None
+
+    def start(self, function, decoding_format, SECURITY_KEY):
+        """ This function is responsible to start the whole loop of request and response, this one will take a
+        argument regarding what function to run when dealt with a request. the input functions must have one
+        argument, which is the request, and must return a response"""
+
+        def client(conn):
+            while True:
+                request = conn.recv(8100).decode(decoding_format)
+                if SECURITY_KEY in request:
+                    conn.send(function(request))
+
+        self.server.listen()
+        while True:
+            connection, address = self.server.accept()
+            thread = threading.Thread(target=client, args=connection)
+            thread.start()
+
+    def close(self):
+        """ This function is here to shut down the server when needed. """
+        self.server.close()
